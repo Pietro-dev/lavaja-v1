0cd8faaf763729ba789806b27983e39e8459a02e
Pietro-dev <pietro.pietromartins@gmail.com>
2026-07-12 12:40:59 -0300

    feat: torna atributos do dto obrigatórios, assim como na entity

--- src/main/java/com/lavaja/saas/backend/dtos/UsuarioInputDTO.java
+++ src/main/java/com/lavaja/saas/backend/dtos/UsuarioInputDTO.java
@@ -1,8 +1,15 @@
 package com.lavaja.saas.backend.dtos;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UsuarioInputDTO (
+        @NotBlank(message = "Insira um nome de usuário!")
         String nome,
+        @NotBlank(message = "Crie uma senha válida") @Size(min = 6)
         String senha,
+        @NotBlank @Column(unique = true)
         String email
 ) {}
 
